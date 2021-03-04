48e0f80746412c0f32e3ab0271bc9eeceb902895
Miloiu Marian Iulian <miloiu_marian@yahoo.com>
2021-03-04 16:16:41 +0200

    UmbracoApi

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,54 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+using comandamancare.Models;
+
+
+namespace comandamancare.Controllers
+{
+    public class PeopleController : UmbracoApiController //ApiController 
+    {
+        List<Individual> people = new List<Individual>();
+
+        public PeopleController()
+        {
+            people.Add(new Individual { FirstName = "Tim", LastName = "Corey"});
+            people.Add(new Individual { FirstName = "Sue", LastName = "Storm" });
+            people.Add(new Individual { FirstName = "Bilbo", LastName = "Baggins" });
+        }
+
+
+        // GET: api/People
+        [HttpGet]
+        public List<Individual> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/People
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EnableRouting/myComposer.cs
@@ -0,0 +1,14 @@
+﻿using comandamancare.RegisterSettings;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace comandamancare.EnableRouting
+{
+    public class myComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<ResterSettingsComponent>();
+        }
+    }
+}
